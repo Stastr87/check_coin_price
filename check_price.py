3e8126dd68f7ed998d49ddef096135da121204a7
Stastr87 <stastr87@gmail.com>
2022-06-01 23:41:06 +0400

    перенес весь код в check_quotes.py

--- check_price.py
+++ check_price.py
@@ -6,6 +6,8 @@ import os
 from pprint import pprint
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s_%(levelname)s: %(message)s')
+
+#Есть
 class Coin_obj(object):
     def __init__(self, coin_name=None,price_A=None,timestamp_A=None,price_B=None,timestamp_B=None):
         self.coin_name=coin_name
@@ -29,83 +31,74 @@ class Coin_obj(object):
         data['price_moving']=self.price_moving
         return data
 
-
+#Есть
 def get_config():
     with open('config.json', 'r') as config_file:
         config=json.loads(config_file.read())
         config_file.close()
         return config
 
-
+#Есть
 def get_quotes_data():
     with open(os.path.join('.','temp','response_data.json'), 'r') as quotes_file:
         quotes=json.loads(quotes_file.read())
         quotes_file.close()
     return quotes
-
+#Не требуется
 def save_data(json_data):    #Сохранение данных json в файл
     with open(os.path.join('.','temp','output_data.json'), 'w', encoding='utf-8') as output_data_file:
         output_data_file.write(json.dumps(json_data, indent=4, sort_keys=True))
         output_data_file.close()
 
-
+#Есть
 def create_new_coin_list(quotes_list):
     coin_list=[]
     for item in quotes_list:
         coin_list.append(Coin_obj(item['symbol'], item['price'],item['time']).to_json())
     return coin_list
-
+# Есть
 def calculate_price_moving(some_coin_list):
     for item in some_coin_list:
         item['price_moving']=Coin_obj.get_price_moving(item['price_A'],item['price_B'])
     return some_coin_list
-
+# Есть
 def check_price(quotes_json_data, coin):
     for item in quotes_json_data:
         if item['symbol']==coin:
             price=item['price']
             time_stmp=item['time']
     return price, time_stmp
-
+# Есть
 def retry(func):    #Декоратор функции в котором выполняется бесконечный цикл запросов
-    def wrappedFunc(self, *args, **kwargs):
+    def wrappedFunc(*args, **kwargs):
         while True:
             logging.debug(f'{func.__name__}() called')
-            func(self, *args, **kwargs)
-            time.sleep(59)
+            func(*args, **kwargs)
+            time.sleep(1)
     return wrappedFunc
 
-#@retry
-## Реализовать обновление списка наблюдения 
-#def update_coin_list
+@retry
+def update_coin_list():
+    config=get_config()    #Подгрузить актуальные настройки
+    quotes_list=get_quotes_data()    #Получить исходные данные
+    coin_list=create_new_coin_list(quotes_list)     #Создать новый список с данными для обработки
+
+    for coin in coin_list:    #Зафиксировать цену вначале таймфрейма
+        coin['price_A'], coin['timestamp_A']=check_price(quotes_list, coin['coin_name'])
+    
+    time.sleep(int(config['time_frame']))    #Время ожидания до следующей итерации
+    quotes=get_quotes_data()    # Получить свежие котировки
+    
+    for coin in coin_list:    #Зафиксировать цену вконце таймфрейма
+        coin['price_B'], coin['timestamp_B']=check_price(quotes, coin['coin_name'])
+
+    save_data(calculate_price_moving(coin_list))    #Подсчитать разницу и сохранить файл
+    logging.debug(f'Coin list updated!')
 
 ##--------------Основной код---------------
-config=get_config()
 
-quotes_list=get_quotes_data()    #Получить исходные данные
-
-coin_list=create_new_coin_list(quotes_list)     #Создать новый список с данными для обработки
-
-for coin in coin_list:    #Зафиксировать цену вначале таймфрейма
-    price, time_stmp=check_price(quotes_list, coin['coin_name'])
-    coin['price_A']=price
-    coin['timestamp_A']=time_stmp
-
-save_data(coin_list)
-
-time.sleep(int(config['time_frame']))    #Время ожидания до следующей итерации
-
-quotes=get_quotes_data()    # Получить свежие котировки
-
-for coin in coin_list:    #Зафиксировать цену вконце таймфрейма
-    price, time_stmp=check_price(quotes, coin['coin_name'])
-    coin['price_B']=price
-    coin['timestamp_B']=time_stmp
-save_data(coin_list)
-
-
-save_data(calculate_price_moving(coin_list))    #Подсчитать разницу и сохранить файл
+update_coin_list()
 
 
 
-logging.debug(f'output_data.json обновлен')
+
--- check_quotes.py
+++ check_quotes.py
@@ -7,6 +7,29 @@ from pprint import pprint
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s_%(levelname)s: %(message)s')
 
+class Coin_obj(object):
+    def __init__(self, coin_name=None,price_A=None,timestamp_A=None,price_B=None,timestamp_B=None):
+        self.coin_name=coin_name
+        self.price_A=price_A
+        self.timestamp_A=timestamp_A
+        self.price_B=price_B
+        self.timestamp_B=timestamp_B
+        self.price_moving=None
+
+    def get_price_moving(price_A, price_B):
+        price_moving=(float(price_A)-float(price_B))/float(price_B)
+        return price_moving
+
+    def to_json(self):
+        data={}
+        data['coin_name']=self.coin_name
+        data['price_A']=self.price_A
+        data['timestamp_A']=self.timestamp_A
+        data['price_B']=self.price_B
+        data['timestamp_B']=self.timestamp_B
+        data['price_moving']=self.price_moving
+        return data
+
 def get_config():
     with open('config.json', 'r') as config_file:
         config=json.loads(config_file.read())
@@ -35,40 +58,81 @@ def check_quotes():    #Функция запроса котировок
 
 
 
-def save_data(json_data):    #Сохранение данных json в файл
+def save_data(json_data, file_name):    #Сохранение данных json в файл
     if not os.path.exists('temp'): os.makedirs('temp') 
-    with open(os.path.join('.','temp','response_data.json'), 'w') as response_data_file:
+    with open(os.path.join('.','temp',file_name), 'w') as response_data_file:
         response_data_file.write(json.dumps(json_data, indent=4, sort_keys=True))
         response_data_file.close()
 
-@retry
-#def update_coin_list(*data, coin_list={}):
-def update_coin_list():
-    response_data, response_code=check_quotes()
-    quotes=response_data
-    #result_data=data[0]
-    #quotes=result_data[0]    #парсинг запроса 
-    #quotes=result_data     #debug string
-    #response_code=result_data[1]
-    #response_code=200    # DEBUG string
-
+def coin_filter(quotes,response_code):
     if response_code==200:
        try:
            for item in quotes:
-               if item['symbol'] in config['waste_coin_list']:    #Удалить из списка определенные монеты
+               if item['symbol'] in get_config()['waste_coin_list']:    #Удалить из списка определенные монеты
                    quotes.remove(item)
-               if item['symbol'][-4:] in config['coin_mask']:    #Удалить из списка монеты по заданной маске
+               if item['symbol'][-4:] in get_config()['coin_mask']:    #Удалить из списка монеты по заданной маске
                    quotes.remove(item)
-           save_data(quotes)    # Сохраняются данные с примененым фильтром 
+           save_data(quotes, 'response_data.json')    # Сохраняются данные с примененым фильтром 
            logging.debug(f'Данные обновлены')
        except:
-           save_data(quotes)
+           save_data(quotes,'response_data.json')
            logging.debug(f'Ошибка фильтрации, но данные обновлены')
     else:
         logging.debug(f'Ошибка получения данных от сервера, response_code: {response_code}')
+    return quotes
+
+def create_new_coin_list(some_coin_list):
+    new_coin_list=[]
+    for item in some_coin_list:
+        new_coin_list.append(Coin_obj(item['symbol'], item['price'],item['time']).to_json())
+    return new_coin_list
+
+def check_price(quotes_json_data, coin):
+    for item in quotes_json_data:
+        if item['symbol']==coin:
+            price=item['price']
+            time_stmp=item['time']
+        else:
+            pass
+            #price=None
+            #time_stmp=None
+    return price, time_stmp
+
+def calculate_price_moving(some_coin_list):
+    for item in some_coin_list:
+        item['price_moving']=Coin_obj.get_price_moving(item['price_A'],item['price_B'])
+    return some_coin_list
+
+@retry
+def update_coin_list():
+    response_data, response_code=check_quotes()
+    actual_quotes=coin_filter(response_data, response_code)
+
+    coin_list=create_new_coin_list(actual_quotes)     #Создать новый список с данными для обработки
+
+    for coin in coin_list:    #Зафиксировать цену вначале таймфрейма
+        coin['price_A'], coin['timestamp_A']=check_price(actual_quotes, coin['coin_name'])
+
+    
+    time.sleep(int(get_config()['time_frame']))    #Время ожидания до следующей итерации
+
+    response_data, response_code=check_quotes()    # Получить свежие котировки
+    actual_quotes=coin_filter(response_data, response_code)
+
+    actual_quotes_tikers=[]    #Для проверки того что в новых котировках имеется тикер для сравнения
+    for quote in actual_quotes:
+        actual_quotes_tikers.append(quote["symbol"])
+
+    for coin in coin_list:    #Зафиксировать цену вконце таймфрейма
+        if coin['coin_name'] in actual_quotes_tikers:    #Проверка на наличие в котировках тикер для сравнения
+            coin['price_B'], coin['timestamp_B']=check_price(actual_quotes, coin['coin_name'])
+    save_data(coin_list,'output_data.json')
+
+    save_data(calculate_price_moving(coin_list),'calculated_output_data.json')    #Подсчитать разницу и сохранить файл
+
+    logging.debug(f'Coin list updated!')
 
 ##------исполняемый код скрипта-------
-config=get_config()
-host=config["host"]
 
+host=get_config()["host"]
 update_coin_list()
